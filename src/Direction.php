3c9e3fb8ee1ffbf38bee814a301b3a3c6b6ddb05
Nail' Gafarov <nil20122013@gmail.com>
2022-05-13 13:12:02 +0500

    class -> enum

--- src/Direction.php
+++ src/Direction.php
@@ -1,24 +1,14 @@
 <?php
-	namespace Stein197;
+namespace Stein197;
 
-	/**
-	 * Enum-like class that contains only four public constants. They could be combined with each other by using bitwise
-	 * OR. This class Should be used whenever direction or side is implied. It cannot be instantiated.
-	 * @package Stein197
-	 */
-	final class Direction {
-
-		/** Upper direction */
-		public const UP = 1 << 0;
-		/** Down direction */
-		public const DOWN = 1 << 1;
-		/** Left direction */
-		public const LEFT = 1 << 2;
-		/** Right direction */
-		public const RIGHT = 1 << 3;
-
-		/**
-		 * Prevent instantiating
-		 */
-		private function __construct() {}
-	}
+/**
+ * Enum that contains only four cases. They could be combined with each other by using bitwise OR. This enum should be
+ * used whenever direction or side is implied.
+ * @package Stein197
+ */
+enum Direction: int {
+	case Up    = 1 << 0;
+	case Down  = 1 << 1;
+	case Left  = 1 << 2;
+	case Right = 1 << 3;
+}
